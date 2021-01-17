4ef5b7b07c3ee10f11deb3e740269bcc7fb3060d
sandeep <sandeep.guntupalli567@gmail.com>
2021-01-17 19:30:17 +0530

    implemented loan eligibility calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,43 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+const STANDARD_INTEREST = 10;
+
+interface displayLoanObject {
+  finalAmount,
+  emi
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   form: FormGroup;
-
-  constructor(
-    private fb: FormBuilder,
-  ) {}
+  public emiObj:displayLoanObject;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.fb.group({
-      color: 20
+      income: 150000,
+      expense: 50000,
+      repayment: '6',
+      previous_emi: true,
+      monthly_emi: 15000,
     });
+    this.calculateEMI();
+  }
+  calculateEMI() {
+    let loanAmount = this.form.value.income - this.form.value.monthly_emi;
+    let term = this.form.value.repayment;
+    let top = Math.pow(1 + STANDARD_INTEREST/1200, term);
+    let bottom = top - 1;
+    let ratio = top / bottom;
+    let EMI = +loanAmount * +STANDARD_INTEREST/1200 * +ratio;
+    let Total = +EMI * +term;
+    this.emiObj = {
+      emi: EMI.toFixed(0),
+      finalAmount: Total.toFixed(0),
+    };
   }
 }
